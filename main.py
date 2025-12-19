8979059e5a633b6375e66ce6b3b591115d3980be
TrevorBrowning <trevryanbrowning@gmail.com>
2025-12-19 12:22:19 -0500

    feat: added word frequency

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_book_text, word_count, char_count, sort_stats
+from stats import get_book_text, word_count, char_count, sort_stats, sort_word_frequency, word_frequency_analysis
 import sys
 
 def main():
@@ -12,20 +12,56 @@ def main():
     path = sys.argv[1]
     book_text = get_book_text(path)
     word_counter = word_count(book_text)
+    
+    
     char_counter = char_count(book_text)
     stats_sorter = sort_stats(char_counter)
+    char_filtered = char_filter(stats_sorter)
 
+
+    char_slice = slice(0,10)
+    
+    
+    word_analysis = word_frequency_analysis(book_text)
+    word_frequency = sort_word_frequency(word_analysis)
+    word_slice = slice(0,10)
+    
     print("============ BOOKBOT ============")
+    print()
     print(f"Analyzing book found at {path}...")
+    print()
     print("----------- Word Count ----------")
+    print()
     print(f"Found {word_counter} total words")
-    print("--------- Character Count -------")
+    print()
+    print("--------- Most Common Characters -------")
+    print()
+    
+    num_char_list = 0
+    for item in char_filtered[char_slice]:
+        num_char_list += 1
+        char = item["char"]
+        num = item["num"]
+        print(f"{num_char_list}: {char} - {num}")
+    print()
+    print("============= Most Common Words ===============")
+    print()
+    num_word_list = 0
+    for i in word_frequency[word_slice]:
+        word = i["word"]
+        amount = i["num"]
+        num_word_list += 1
+        print(f"{num_word_list}: {word} - {amount}")
+    print()
+    print("============= END ===============")
+
+def char_filter(stats_sorter):
+    char_filtered_list = []
     for item in stats_sorter:
         char = item["char"]
         num = item["num"]
-
         if char.isalpha():
-            print(f"{char}: {num}")
-    print("============= END ===============")
-
+            char_filtered_list.append(item)
+    return char_filtered_list
+    
 main()
--- stats.py
+++ stats.py
@@ -1,15 +1,17 @@
+
+# Get book data
 def get_book_text(book_path):
     with open(book_path) as f:
         file_contents = f.read()
         return file_contents
 
-
+# Count book words
 def word_count(file_contents):
     book_text = file_contents.split()
     num_words = len(book_text)
     return num_words
 
-
+# Count book characters
 def char_count(file_contents):
     char_counter = {}
     book_text = file_contents.lower()
@@ -27,7 +29,7 @@ def char_count(file_contents):
 def sort_helper(item):
     return item["num"]
 
-
+# Sort book characters by amount 
 def sort_stats(char_counter):
     sorted_dictionary = []
     for key, value in char_counter.items():
@@ -38,4 +40,33 @@ def sort_stats(char_counter):
         sorted_dictionary.append(sort_dict)
     sorted_dictionary.sort(reverse=True, key=sort_helper)
     return sorted_dictionary
+
+
+def word_frequency_analysis(file_contents):
+
+    frequency_dict = {}
+
+    book_words = file_contents.split()
+    for i in book_words:
+        i = i.lower().strip('.,!?[]"()')
+        if i not in frequency_dict:
+            frequency_dict[i] = 1
+        else:
+            frequency_dict[i] += 1
+
+    return frequency_dict
+
+
+def sort_word_frequency(word_frequency_analysis):
+    sorted_word_dict = []
+    for key, value in word_frequency_analysis.items():
+        sort_dict = {
+            "word": key,
+            "num": value,
+        }
+        sorted_word_dict.append(sort_dict)
+    sorted_word_dict.sort(reverse=True, key=sort_helper)
+    return sorted_word_dict
+    
+
  
